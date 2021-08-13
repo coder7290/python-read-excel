03d66294069de24e5b91082a12ca892d7dbf0d65
coder7290 <topdev0729@gmail.com>
2021-08-14 04:40:58 +0900

    feat: add read excel file function

--- script.py
+++ script.py
@@ -1,2 +1,24 @@
-input pandas as pd
+import pandas as pd
 
+def read_excel():
+    user_data = pd.read_excel(r"fatigue_risk_calc(decrypted).xlsm", 
+    engine='openpyxl',
+    sheet_name="Schedule", 
+    usecols="A:L",
+    header=9).values.tolist()
+    for row in user_data:
+        day = row[0]
+        on_duty = row[1]
+        off_duty = row[2]
+        job_type_breaks = row[3]
+        commuting_time = row[4]
+        duty_length = row[5]
+        rest_length = row[6]
+        average_duty_per_day = row[6]
+        cumulative_component = row[8]
+        duty_timing_component = row[9]
+        job_type_breaks_component = row[10]
+        fatigue_index = row[11]
+        print(row[1])
+        
+read_excel()
