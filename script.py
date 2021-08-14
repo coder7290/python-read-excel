6d8e7828d2ede43d5bb23d6b1c49a567ec8ce258
coder7290 <topdev0729@gmail.com>
2021-08-14 13:54:54 +0900

    feat: save data

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sqlite3
 
 def read_excel():
     user_data = pd.read_excel(r"fatigue_risk_calc(decrypted).xlsm", 
@@ -6,19 +7,55 @@ def read_excel():
     sheet_name="Schedule", 
     usecols="A:L",
     header=9).values.tolist()
-    for row in user_data:
-        day = row[0]
-        on_duty = row[1]
-        off_duty = row[2]
-        job_type_breaks = row[3]
-        commuting_time = row[4]
-        duty_length = row[5]
-        rest_length = row[6]
-        average_duty_per_day = row[6]
-        cumulative_component = row[8]
-        duty_timing_component = row[9]
-        job_type_breaks_component = row[10]
-        fatigue_index = row[11]
-        print(row[1])
-        
-read_excel()
+
+    return user_data
+
+def save_data():
+    try:
+        conn = sqlite3.connect("fatigue.db")
+    except:
+        print("Database connection error!")
+
+    sql = '''INSERT INTO fatigue(day, on_duty, 
+    off_duty, job_type_breaks, commuting_time, duty_length, 
+    rest_length, average_duty_per_day, cumulative_component, 
+    duty_timing_component, job_type_breaks_component, fatigue_index)
+    VALUES'''
+
+    day = ""
+    on_duty = ""
+    off_duty = ""
+    job_type_breaks = ""
+    commuting_time = ""
+    duty_length = ""
+    rest_length = ""
+    average_duty_per_day = ""
+    cumulative_component = ""
+    duty_timing_component = ""
+    job_type_breaks_component = ""
+    fatigue_index = ""
+
+    user_data = read_excel()
+    for index, row in enumerate(user_data):
+        day = str(row[0])
+        on_duty = str(row[1])
+        off_duty = str(row[2])
+        job_type_breaks = str(row[3])
+        commuting_time = str(row[4])
+        duty_length = str(row[5])
+        rest_length = str(row[6])
+        average_duty_per_day = str(row[6])
+        cumulative_component = str(row[8])
+        duty_timing_component = str(row[9])
+        job_type_breaks_component = str(row[10])
+        fatigue_index = str(row[11])
+        if index != (len(user_data) - 1) :
+            sql += '(' + f'"{day}"' + ',' + f'"{on_duty}"' + ',' + f'"{off_duty}"' + ',' + f'"{job_type_breaks}"' + ',' + f'"{commuting_time}"' + ',' + f'"{duty_length}"' + ',' + f'"{rest_length}"' + ',' + f'"{average_duty_per_day}"' + ',' + f'"{cumulative_component}"' + ',' + f'"{duty_timing_component}"' + ',' + f'"{job_type_breaks_component}"' + ',' + f'"{fatigue_index}"' + ')' + ','
+        else :
+            sql += '(' + f'"{day}"' + ',' + f'"{on_duty}"' + ',' + f'"{off_duty}"' + ',' + f'"{job_type_breaks}"' + ',' + f'"{commuting_time}"' + ',' + f'"{duty_length}"' + ',' + f'"{rest_length}"' + ',' + f'"{average_duty_per_day}"' + ',' + f'"{cumulative_component}"' + ',' + f'"{duty_timing_component}"' + ',' + f'"{job_type_breaks_component}"' + ',' + f'"{fatigue_index}"' + ')'
+        # print(row[1])
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+
+save_data()
