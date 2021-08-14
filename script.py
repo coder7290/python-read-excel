643f55c547083673dadb8a081e30bb3f772a210f
coder7290 <topdev0729@gmail.com>
2021-08-14 16:30:14 +0900

    feat: crate exe file

--- script.py
+++ script.py
@@ -69,6 +69,25 @@ def save_data():
         job_type_breaks_component = str(row[10])
         fatigue_index = str(row[11])
         cur = conn.cursor()
+        create_table_query = '''
+                CREATE TABLE IF NOT EXISTS "fatigue" (
+                    "id"	INTEGER NOT NULL,
+                    "day"	TEXT,
+                    "on_duty"	REAL,
+                    "off_duty"	TEXT,
+                    "job_type_breaks"	TEXT,
+                    "commuting_time"	TEXT,
+                    "duty_length"	TEXT,
+                    "rest_length"	TEXT,
+                    "average_duty_per_day"	TEXT,
+                    "cumulative_component"	TEXT,
+                    "duty_timing_component"	TEXT,
+                    "job_type_breaks_component"	TEXT,
+                    "fatigue_index"	TEXT,
+                    PRIMARY KEY("id" AUTOINCREMENT)
+                )'''
+        cur.execute(create_table_query)
+        
         compare_query = "SELECT * FROM fatigue WHERE day = " + f"'{day}'"
         exist_data = cur.execute(compare_query).fetchone()
         if exist_data == None : 
@@ -77,10 +96,8 @@ def save_data():
                 rest_length, average_duty_per_day, cumulative_component, 
                 duty_timing_component, job_type_breaks_component, fatigue_index)
                 VALUES'''
-            if index != (len(user_data) - 1) :
-                sql += '(' + f'"{day}"' + ', ' + f'"{on_duty}"' + ', ' + f'"{off_duty}"' + ', ' + f'"{job_type_breaks}"' + ', ' + f'"{commuting_time}"' + ', ' + f'"{duty_length}"' + ', ' + f'"{rest_length}"' + ', ' + f'"{average_duty_per_day}"' + ', ' + f'"{cumulative_component}"' + ', ' + f'"{duty_timing_component}"' + ', ' + f'"{job_type_breaks_component}"' + ', ' + f'"{fatigue_index}"' + ')' + ', '
-            else :
-                sql += '(' + f'"{day}"' + ', ' + f'"{on_duty}"' + ', ' + f'"{off_duty}"' + ', ' + f'"{job_type_breaks}"' + ', ' + f'"{commuting_time}"' + ', ' + f'"{duty_length}"' + ', ' + f'"{rest_length}"' + ', ' + f'"{average_duty_per_day}"' + ', ' + f'"{cumulative_component}"' + ', ' + f'"{duty_timing_component}"' + ', ' + f'"{job_type_breaks_component}"' + ', ' + f'"{fatigue_index}"' + ')'
+
+            sql += '(' + f'"{day}"' + ', ' + f'"{on_duty}"' + ', ' + f'"{off_duty}"' + ', ' + f'"{job_type_breaks}"' + ', ' + f'"{commuting_time}"' + ', ' + f'"{duty_length}"' + ', ' + f'"{rest_length}"' + ', ' + f'"{average_duty_per_day}"' + ', ' + f'"{cumulative_component}"' + ', ' + f'"{duty_timing_component}"' + ', ' + f'"{job_type_breaks_component}"' + ', ' + f'"{fatigue_index}"' + ')'
 
             try:
                 cur.execute(sql)
@@ -91,12 +108,10 @@ def save_data():
             sql = '''UPDATE fatigue SET '''
             sql += 'day = ' + f'"{day}"' + ', ' + 'on_duty = ' + f'"{on_duty}"' + ', ' + 'off_duty = ' + f'"{off_duty}"' + ', ' + 'job_type_breaks = ' + f'"{job_type_breaks}"' + ', ' + 'commuting_time = ' + f'"{commuting_time}"' + ', ' + 'duty_length = ' + f'"{duty_length}"' + ', ' + 'rest_length = ' + f'"{rest_length}"' + ', ' + 'average_duty_per_day = ' + f'"{average_duty_per_day}"' + ', ' + 'cumulative_component = ' + f'"{cumulative_component}"' + ', ' + 'duty_timing_component = ' + f'"{duty_timing_component}"' + ', ' + 'job_type_breaks_component = ' + f'"{job_type_breaks_component}"' + ', ' + 'fatigue_index = ' + f'"{fatigue_index}"'            
             sql += f''' WHERE day = "{day}"'''
-
             try:
                 cur.execute(sql)
             except sqlite3.Error as e:
                 print(e)
-
     try :
         conn.commit()
         popup_showinfo()
